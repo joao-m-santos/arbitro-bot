c18a910abc794a39612141eb4e1d1cf2543e9918
João Santos <jm_santos7@hotmail.com>
2023-08-09 15:27:08 +0200

    Add typescript

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,9 @@
-import { Events } from 'discord.js';
+import { Client, Events } from 'discord.js';
 
 export default {
   name: Events.ClientReady,
   once: true,
-  execute(client) {
+  execute(client: Client<true>) {
     console.log(`\nReady! Logged in as ${client.user.tag}`);
   },
 };
